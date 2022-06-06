ae64a67d66a31b6e543ff87d8cc77a2b64ca2f7e
dding-g <area409@gmail.com>
2022-06-06 22:18:46 +0900

    Updates

---
+++ component---src-pages-404-js-27981ca7dd461cb4280e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{429:function(t,e,a){a.r(e);var n=a(7294),l=a(8678),s=a(262);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-27981ca7dd461cb4280e.js.map
