1183571722ef5eb71a90d3aaa915ef20ffb4d91c
dding-g <area409@gmail.com>
2022-07-12 18:40:39 +0900

    favicon 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,7 +121,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
